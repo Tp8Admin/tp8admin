abe14f0c0fe09d220b983d0e7ab14a0ac15f3b57
YangHongwei <403236160@qq.com>
2023-08-28 00:06:25 +0800

    安装基本方法的制作

---
+++ app/api/controller/Install.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace app\api\controller;
+
+
+use app\common\controller\Api;
+use ta\Version;
+
+class Install extends Api
+{
+    /**
+     * 环境基础检查
+     */
+    public function envBaseCheck()
+    {
+         
+
+
+    }
+}
