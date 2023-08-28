b575b4456a34536f8edd28747b42fccebb85f6e1
YangHongwei <403236160@qq.com>
2023-08-28 15:50:36 +0800

    制作接口基类制作输出成功和失败的接口

---
+++ app/api/controller/Demo.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\api\controller;
+
+use app\common\controller\Api;
+
+class Demo extends Api
+{
+	/**
+	 * 接口成功的示例
+	 */
+	public function demoSuccess()
+	{
+		$this->success('操作成功',
+			['id' => 1, 'name' => '张三']
+		);
+	}
+
+	/**
+	 * 接口失败的示例
+	 */
+	public function demoError()
+	{
+		$this->error('操作失败');
+	}
+}
