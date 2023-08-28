868ce24da82549875ecd270b8320e13832f87702
YangHongwei <403236160@qq.com>
2023-08-28 16:16:28 +0800

    检测PHP版本

--- app/api/controller/Install.php
+++ app/api/controller/Install.php
@@ -5,17 +5,28 @@ namespace app\api\controller;
 
 
 use app\common\controller\Api;
-use ta\Version;
 
 class Install extends Api
 {
-    /**
-     * 环境基础检查
-     */
-    public function envBaseCheck()
-    {
-         
+	/**
+	 * 环境基础检查
+	 */
+	public function envBaseCheck()
+	{
+		// 1. 检测php版本
+		$phpVersion        = phpversion();
+		$phpVersionCompare = version_compare($phpVersion, '8.2.0', '<');
+		if ($phpVersionCompare) {
+			$this->error('PHP版本必须大于等于8.2.0');
+		}
 
-
-    }
+		// 输出
+		$this->success('', [
+			'php_version' => [
+				'describe' => $phpVersion,
+				'state'    => $phpVersionCompare ? 'fail' : 'ok',
+				'link'     => $phpVersionLink ?? [],
+			],
+		]);
+	}
 }
