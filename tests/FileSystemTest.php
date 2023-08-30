bd543c8dd5f66212da17d484641e93742dcef425
YangHongwei <403236160@qq.com>
2023-08-30 16:37:46 +0800

    test:增加phpunit单元测试

---
+++ tests/FileSystemTest.php
@@ -0,0 +1,18 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use ta\FileSystem;
+
+class FileSystemTest extends TestCase
+{
+	public function testPathIsWritable()
+	{
+		// 测试文件夹
+		$writableFolder = FileSystem::pathIsWritable('/Applications/EasySrv/www/tp8admin/config');
+		$this->assertTrue($writableFolder);
+
+		// 测试文件
+		$writableFile = FileSystem::pathIsWritable('/Applications/EasySrv/www/tp8admin/config/database.php');
+		$this->assertTrue($writableFile);
+	}
+
+}
