6bd60ca755228a3c6427b6fdb02f7f7a70e33762
YangHongwei <403236160@qq.com>
2023-09-01 19:43:05 +0800

    perf:跨域问题的解决及跨域配置以及一些BUG的优化

--- app/api/controller/Install.php
+++ app/api/controller/Install.php
@@ -17,12 +17,26 @@ class Install extends Api
         $phpVersion        = phpversion();
         $phpVersionCompare = version_compare($phpVersion, '8.2.0', '<');
         if ($phpVersionCompare) {
-            $this->error('PHP版本必须大于等于8.2.0');
+            $phpVersionLink = [
+                [
+                    // 需要PHP版本
+                    'name' => '需要 >= 8.0.2',
+                    'type' => 'text'
+                ],
+                [
+                    // 如何解决
+                    'name'  => '如何解决',
+                    'title' => '点击查看如何解决？',
+                    'type'  => 'faq',
+                    'url'   => 'https://wonderful-code.gitee.io/guide/install/preparePHP.html'
+                ]
+            ];
         }
 
         // 输出
         $this->success('', [
             'php_version' => [
+                'name'     => 'PHP版本',
                 'describe' => $phpVersion,
                 'state'    => $phpVersionCompare ? 'fail' : 'ok',
                 'link'     => $phpVersionLink ?? [],
---
+++ app/middleware/AllowCrossDomain.php
@@ -0,0 +1,51 @@
+<?php
+declare (strict_types=1);
+
+namespace app\middleware;
+
+use Closure;
+use think\Request;
+use think\Response;
+use think\facade\Config;
+
+/**
+ * 跨域请求支持
+ * 安全起见，只支持了配置中的域名
+ */
+class AllowCrossDomain
+{
+    protected array $header = [
+        'Access-Control-Allow-Credentials' => 'true',
+        'Access-Control-Max-Age'           => 1800,
+        'Access-Control-Allow-Methods'     => 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers'     => 'think-lang, server, ba-user-token, tatoken, Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-CSRF-TOKEN, X-Requested-With',
+    ];
+
+    /**
+     * 跨域请求检测
+     * @access public
+     * @param Request    $request
+     * @param Closure    $next
+     * @param array|null $header
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next, ?array $header = []): Response
+    {
+        $header = !empty($header) ? array_merge($this->header, $header) : $this->header;
+
+        $origin = $request->header('origin');
+        if ($origin) {
+            $info = parse_url($origin);
+
+            // 获取跨域配置
+            $corsDomain   = explode(',', Config::get('tp8admin.cors_request_domain'));
+            $corsDomain[] = $request->host(true);
+
+            if (in_array("*", $corsDomain) || in_array($origin, $corsDomain) || (isset($info['host']) && in_array($info['host'], $corsDomain))) {
+                header("Access-Control-Allow-Origin: " . $origin);
+            }
+        }
+
+        return $next($request)->header($header);
+    }
+}
